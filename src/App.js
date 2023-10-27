91d68e746df4d9959f627d013dad54892863e775
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-10-27 22:46:13 +0530

    'background-color-changer'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [backGroundColor,setBackGroundColor] = useState('')
+
+  const backGroundColorChange = (color) => {
+    setBackGroundColor(color)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{width:'100%',height:'100vh',display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',backgroundColor:backGroundColor}}>
+        <h1>Background Color Change</h1>
+        <div style={{display:"flex",justifyContent:'center',alignItems:'center',marginTop:"4rem",gap:'2rem'}}>
+          <button onClick={() => backGroundColorChange('red')} style={{border:'1px solid red',backgroundColor:'red',color:"white",padding:'0.6rem 1rem',borderRadius:"8px"}}>Red</button>
+          <button onClick={() => backGroundColorChange('blue')} style={{border:'1px solid blue',backgroundColor:'blue',color:"white",padding:'0.6rem 1rem',borderRadius:"8px"}}>Blue</button>
+          <button onClick={() => backGroundColorChange('green')} style={{border:'1px solid green',backgroundColor:'green',color:"white",padding:'0.6rem 1rem',borderRadius:"8px"}}>Green</button>
+          <button onClick={() => backGroundColorChange('yellow')} style={{border:'1px solid yellow',backgroundColor:'yellow',color:"white",padding:'0.6rem 1rem',borderRadius:"8px"}}>Yellow</button>
+        </div>
+      </div>
+    </>
   );
 }
 
